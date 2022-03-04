0b1abe9f7df15612894a4da5f64ac8d99499f9a9
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-03-04 10:21:54 +0000

    act

--- src/main/java/Ficha5/LoadFromFile.java
+++ src/main/java/Ficha5/LoadFromFile.java
@@ -1,13 +1,24 @@
 package Ficha5;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class LoadFromFile {
-
-	public void obterInformacaoFicheiro(String fileName)
+	
+	public List<Pessoa> obterInformacaoFicheiro(String fileName)
 	{
-		getFileAsIOStream(fileName);
+		
+		return convertStringToPessoa(
+				getFileContent(
+						getFileAsIOStream(fileName)));
 		
 	}
 	
@@ -25,14 +36,51 @@ public class LoadFromFile {
 		    return ioStream;
 	}
 	
-	private void getFileContent()
+	private String getFileContent(InputStream is)
 	{
+		InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
 		
+        String line;
+        String fileContents = "";
+        try {
+			while ((line = br.readLine()) != null) {
+			    fileContents += line;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        try {
+			is.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return fileContents;
 	}
 	
-	private void convertStringToPessoa()
+	private List<Pessoa> convertStringToPessoa(String fileContents)
 	{
+		JSONArray jArray = new JSONArray(fileContents);
 		
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
+		for (int i = 0; i < jArray.length() ; i++)
+		{
+			Pessoa pessoa = new Pessoa(
+						jArray.getJSONObject(i).getString("Primeiro_nome"),
+						jArray.getJSONObject(i).getString("Ultimo_nome"), 
+						jArray.getJSONObject(i).getInt("Idade"));
+			pessoas.add(pessoa);
+			
+		}
+		
+		return pessoas;
+			
 	}
+
 	
 }
--- src/main/java/Ficha5/Main.java
+++ src/main/java/Ficha5/Main.java
@@ -1,27 +1,25 @@
 package Ficha5;
 
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa("Manuel", "Fernandes", 25);
-		p1.setSaldo(15);
-
-		Pessoa p2 = new Pessoa("Maria", "Rodrigues", 46);
-		p2.setSaldo(20);
+		LoadFromFile lff = new LoadFromFile();
 		
-		Pessoa p3 = new Pessoa("José", "Gonçalves", 50);
-		p3.setSaldo(23.27);
+		List<Pessoa> pessoas = lff.obterInformacaoFicheiro("pessoas.json");
+		
+		pessoas.get(0).setSaldo(15);
+		pessoas.get(1).setSaldo(20);
+		pessoas.get(2).setSaldo(23.27);
 		
 		Mercado mercado = new Mercado();
-		mercado.addPessoa(p1);
-		mercado.addPessoa(p2);
-		mercado.addPessoa(p3);
+		mercado.setPessoas(pessoas);
 		
 		System.out.println(mercado.getSaldoTotal());
 		
-		LoadFromFile testFile = new LoadFromFile();
-		testFile.obterInformacaoFicheiro("pessoas.json");
+		System.out.println(mercado.toString());
 
 	}
 
